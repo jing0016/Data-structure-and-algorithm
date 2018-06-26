b1a30be9be03ef5a73becce56ff45649af1799c6
jing0016 <32021491+jing0016@users.noreply.github.com>
2018-06-26 13:39:34 -0400

    commit

---
+++ Array_Manipulation.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jing
+ * Date: 2018/6/26
+ * Time: 11:11 AM
+ */
+
+function arrayManipulation($n, $queries) {
+    $m = count($queries);
+    $arr = array_fill(1,$n,0);
+
+    for($i = 0;$i<$m;$i++)
+    {
+//        for($j = $queries[$i][0];$j<=$queries[$i][1];$j++)
+//        {
+//            $arr[$j] += $queries[$i][2];
+//        }
+
+        $a = $queries[$i][0];
+        $b = $queries[$i][1];
+        $k = $queries[$i][2];
+        $arr[$a] += $k;
+        if(($b+1)<=$n){
+            $arr[$b+1] -= $k;
+        }
+    }
+//    $result = max($arr);
+    $tempMax = 0;
+    $result = 0;
+    for($i = 0; $i < $n;$i++)
+    {
+        $tempMax += $arr[$i];
+        if($tempMax>$result) $result = $tempMax;
+    }
+    return $result;
+}
+
+$n = 5;
+$queries = array(
+    array(1,2,100),
+    array(2,5,100),
+    array(3,4,100),
+);
+
+$result = arrayManipulation($n, $queries);
+echo $result;
